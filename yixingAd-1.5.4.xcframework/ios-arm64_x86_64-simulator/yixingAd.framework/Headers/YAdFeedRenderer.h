9a787099406b3f4d003705a4df89bd47485b25ac
chunzi007 <36952778@qq.com>
2025-11-21 14:57:45 +0800

    chore: 移除旧版yixingAd框架及相关文件
    清理项目中不再使用的旧版yixingAd框架文件，包括1.4.x和1.5.x版本的xcframework及其相关资源文件

--- yixingAd-1.5.4.xcframework/ios-arm64_x86_64-simulator/yixingAd.framework/Headers/YAdFeedRenderer.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-@class YAdFeedAsset;
-
-@interface YAdFeedRenderer : NSObject
-- (void)renderInView:(UIView *)container withAsset:(YAdFeedAsset *)asset layoutType:(NSInteger)layoutType;
-@end
