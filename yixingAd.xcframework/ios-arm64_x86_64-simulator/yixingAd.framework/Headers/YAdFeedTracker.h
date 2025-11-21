4ca7b5f8a61ff8b1a214f1d46e140e52bf71e182
chunzi007 <36952778@qq.com>
2025-11-21 14:52:06 +0800

    release 1.5.5: replace clean yixingAd.xcframework and update podspec

--- yixingAd.xcframework/ios-arm64_x86_64-simulator/yixingAd.framework/Headers/YAdFeedTracker.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface YAdFeedTracker : NSObject
-- (void)startTrackingView:(UIView *)view impression:(void(^)(void))impressionBlock;
-- (void)stop;
-@end
